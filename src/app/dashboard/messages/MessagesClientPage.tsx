92427669d30d1d4090676dd80252b893de4641ff
Dakotah Jennifer <dakjencreativellc@gmail.com>
2025-11-11 17:02:59 -0500

    fix: correctly define primary-accent color in theme

--- src/app/dashboard/messages/MessagesClientPage.tsx
+++ src/app/dashboard/messages/MessagesClientPage.tsx
@@ -260,7 +260,7 @@ export default function MessagesPage({
                 )}
                 <button
                   type="submit"
-                  className="inline-flex justify-center rounded-md border border-transparent bg-red-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-secondary-accent focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="inline-flex justify-center rounded-md border border-transparent bg-primary-accent py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-secondary-accent focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                   Send Message
                 </button>
