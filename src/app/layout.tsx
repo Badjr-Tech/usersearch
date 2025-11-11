de0c710095b314ae774f29bd753340b0dc9f7f83
Dakotah Jennifer <dakjencreativellc@gmail.com>
2025-11-10 19:38:39 -0500

    Remove HTH logos and references

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/hth.ico" />
-      </head>
+              </head>
       <body
         className={`${dmSans.variable} ${bebasNeue.variable} antialiased`}
       >
