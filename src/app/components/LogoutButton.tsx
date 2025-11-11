57b1569d154c0d9948410e64f664bfe34525823e
Dakotah Jennifer <dakjencreativellc@gmail.com>
2025-11-11 16:22:10 -0500

    Set LogoutButton background to green and text to white

--- src/app/components/LogoutButton.tsx
+++ src/app/components/LogoutButton.tsx
@@ -5,7 +5,7 @@ import { logout } from "@/app/login/actions";
 export default function LogoutButton() {
   return (
     <form action={logout}>
-      <button type="submit" className="w-full text-left py-2.5 px-4 rounded transition duration-200 hover:bg-light-gray">
+      <button type="submit" className="w-full text-left py-2.5 px-4 rounded transition duration-200 bg-[#476c2e] text-white hover:bg-light-gray">
         Logout
       </button>
     </form>
