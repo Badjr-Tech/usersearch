8309a609f8d69d536b425fe758e8ae3e4e900ee4
Dakotah Jennifer <dakjencreativellc@gmail.com>
2025-11-11 16:38:18 -0500

    Fix JSX syntax error in MessagesClientPage.tsx by removing extra div

--- src/app/dashboard/messages/MessagesClientPage.tsx
+++ src/app/dashboard/messages/MessagesClientPage.tsx
@@ -359,7 +359,7 @@ export default function MessagesPage({
                       </div>
                     </form>
                   )}
-                </div>      </>
+      </>
     );
   }
             </div>
